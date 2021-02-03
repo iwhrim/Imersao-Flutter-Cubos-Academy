303c866716581e2c5c16e555b037828fbea536dd
Gustavo Teixeira <iwhrim@gmail.com>
2021-02-03 00:02:15 -0300

    Finished initial screen

--- lib/initial_screen.dart
+++ lib/initial_screen.dart
@@ -24,6 +24,31 @@ class InitialScreen extends StatelessWidget {
               fontWeight: FontWeight.w800,
             ),
           ),
+          SizedBox(
+            height: 88.0,
+          ),
+          FlatButton(
+            onPressed: () {
+              print('Começar');
+            },
+            child: Padding(
+              padding: const EdgeInsets.fromLTRB(60, 11, 60, 11),
+              child: Text(
+                'Começar',
+                style: TextStyle(
+                  fontSize: 14,
+                  fontWeight: FontWeight.w600,
+                ),
+              ),
+            ),
+            color: Color(0xffDA0175),
+            textColor: Color(0xffF7F7F7),
+            shape: RoundedRectangleBorder(
+              borderRadius: BorderRadius.circular(
+                20,
+              ),
+            ),
+          ),
         ],
       ),
     );
